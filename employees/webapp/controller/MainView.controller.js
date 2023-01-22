c3c84f3c3b0d59541edbe8085b90a4d8b00bedfd
wcanizalezcassa <walter.canizalez@grupocassa.com>
2023-01-22 02:55:34 +0000

    Laboratorio Layout

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -12,120 +12,42 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("logali.employees.controller.MainView", {
-            onAfterRendering: function () {
-                
-                var oView = this.getView();
-                var oJSONModelEmpl = new sap.ui.model.json.JSONModel();
-             //   var i18nBundle = oView.getModel("i18n").getResourceBundle();
-                oJSONModelEmpl.loadData("./localService/mockdata/Employees.json", false);
-            //    oJSONModel.attachRequestCompleted(function (oEventModel){
-            //        console.log(JSON.stringify(oJSONModel.getData()));
-             //   });
-                oView.setModel(oJSONModelEmpl, "jsonEmployees");
+            onInit: function () {
 
+                var oView = this.getView();
+    
+                // @ts-ignore
+                var oJSONModelEmpl = new sap.ui.model.json.JSONModel();
+                oJSONModelEmpl.loadData("./localService/mockdata/Employees.json", false);
+                oView.setModel(oJSONModelEmpl, "jsonEmployees");
+    
                 var oJSONModelCountries = new sap.ui.model.json.JSONModel();
                 oJSONModelCountries.loadData("./localService/mockdata/Countries.json", false);
                 oView.setModel(oJSONModelCountries, "jsonCountries");
-
+    
+                var oJSONModelLayout = new sap.ui.model.json.JSONModel();
+                oJSONModelLayout.loadData("./localService/mockdata/Layout.json", false);
+                oView.setModel(oJSONModelLayout, "jsonLayout");
+    
                 var oJSONModelConfig = new sap.ui.model.json.JSONModel({
                     visibleID: true,
                     visibleName: true,
-                    visibleCountry:true,
+                    visibleCountry: true,
                     visibleCity: false,
                     visibleBtnShowCity: true,
-                    visibleBtnHideCity:false
-
+                    visibleBtnHideCity: false
                 });
-                oView.setModel(oJSONModelConfig, "jsonConfig");
-
+                oView.setModel(oJSONModelConfig, "jsonModelConfig");
+    
+                this._bus = sap.ui.getCore().getEventBus();
+                this._bus.subscribe("flexible", "showEmployee", this.showEmployeeDetails, this);
             },
-
-            onFilter: function () {
-                var oJSONCountries = this.getView().getModel("jsonCountries").getData();
-                var filters = [];
-
-                if (oJSONCountries.EmployeeId !== "") {
-                    filters.push(new Filter("EmployeeID", FilterOperator.EQ, oJSONCountries.EmployeeId));
-                } else {
-                    
-                }
-
-                if (oJSONCountries.CountryKey !== "") {
-                    filters.push(new Filter("Country", FilterOperator.EQ, oJSONCountries.CountryKey));
-                } else {
-                    
-                }
-
-                var oList = this.getView().byId("tableEmployee");
-                var oBinding = oList.getBinding("items");
-
-                oBinding.filter(filters);
-            },
-
-            onClearFilter: function(){
-                var oModel = this.getView().getModel("jsonCountries");
-                oModel.setProperty("/EmployeeId", "");
-                oModel.setProperty("/CountryKey", "");
-
-            },
-
-            showPostalCode: function(oEvent) {
-                var itemPress = oEvent.getSource();
-                var oContext = itemPress.getBindingContext("jsonEmployees");
-                var objectContext = oContext.getObject();
-
-                sap.m.MessageToast.show(objectContext.PostalCode);
-
-            },
-
-            onValidate: function () {
-                var inputEmployee = this.byId("inputEmployee");
-                var valueEmployee = inputEmployee.getValue();
-
-                if (valueEmployee.length === 6) {
-                    // inputEmployee.setDescription("Ok");
-                    this.byId("labelCountry").setVisible(true);
-                    this.byId("slCountry").setVisible(true);
-
-                } else {
-                    //inputEmployee.setDescription("Not Ok");
-                    this.byId("labelCountry").setVisible(false);
-                    this.byId("slCountry").setVisible(false);
-                };
-            }, 
-
-            onShowCity : function (){
-                var oJSONModelConfig = this.getView().getModel("jsonConfig");
-                oJSONModelConfig.setProperty("/visibleCity",true);
-                oJSONModelConfig.setProperty("/visibleBtnShowCity",false);
-                oJSONModelConfig.setProperty("/visibleBtnHideCity",true);
-            },
-            onHideCity : function(){
-                var oJSONModelConfig = this.getView().getModel("jsonConfig");
-                oJSONModelConfig.setProperty("/visibleCity",false);
-                oJSONModelConfig.setProperty("/visibleBtnShowCity",true);
-                oJSONModelConfig.setProperty("/visibleBtnHideCity",false);
-            }, 
-
-            showOrders : function (oEvent) {
-                //get Selected Controller
-                var iconPressed = oEvent.getSource();
-
-                //Context from the model
-                var oContext = iconPressed.getBindingContext("jsonEmployees");
-                
-                if (!this._oDialogOrders) {
-                    this._oDialogOrders = sap.ui.xmlfragment("logali.employees.fragment.DialogOrders", this);
-                    this.getView().addDependent(this._oDialogOrders);
-                };
-
-                //Dialog binding to the Context to have access to the data of selected item
-                this._oDialogOrders.bindElement("jsonEmployees>" + oContext.getPath());
-                this._oDialogOrders.open();
-              
-            }, 
-            onCloseOrders : function () {
-                this._oDialogOrders.close();
+    
+            showEmployeeDetails: function(category, nameEvent, path) {
+                var detailView = this.getView().byId("detailEmployeeView");
+                detailView.bindElement("jsonEmployees>" + path);
+                this.getView().getModel("jsonLayout").setProperty("/ActiveKey", "TwoColumnsMidExpanded");
             }
+    
         });
     });
